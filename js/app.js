04af658f49dbeee6d5beb339803d0071926bdd5a
Mila Hose <mila.hose@gmail.com>
2017-11-19 13:06:23 -0800

    Add jewel functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-const app = angular.module('store', []);
+(() => {
+  const gems = [
+    {
+      name: 'Dodecahedron', 
+      price: 2.00, 
+      description: '. . .', 
+      canPurchase: true, 
+      soldOut: true
+    }, 
+    {
+      name: 'Pentagonal Gem', 
+      price: 5.95, 
+      description: '. . .', 
+      canPurchase: false
+    }
+  ];
+
+  const app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
+})();
